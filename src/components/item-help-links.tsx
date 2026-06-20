761db00c26e93fb0194d2bbef260e2873a877011
colinzou-git <colin.zou@gmail.com>
2026-06-19 18:23:58 -0700

    Use contextual item help links

--- src/components/item-help-links.tsx
+++ src/components/item-help-links.tsx
@@ -1,5 +1,1 @@
-import Link from "next/link";
-
-export function ItemHelpLinks() {
-  return <div><Link href="/help">Help</Link></div>;
-}
+export { AiChatLinks as ItemHelpLinks } from "./ai-chat-links-v2";
